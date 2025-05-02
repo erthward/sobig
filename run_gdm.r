b955d150cbb5eb1e918942c69e1303fc600934df
Drew Terasaki Hart <drew.terasakihart@csiro.au>
2025-05-02 16:53:16 +1000

    Finished initial pass at the simulator (minus sampling schemes and temporal environmental change). Results look totally whack though. Will debug next week.

---
+++ run_gdm.r
@@ -0,0 +1,40 @@
+library(gdm)
+library(terra)
+
+# get file paths
+args <- commandArgs(trailingOnly = TRUE)
+site_survey_filename = args[1]
+env_rast_filename = args[2]
+spline_filename = args[3]
+pca_rast_filename = args[4]
+
+# read site-survey table
+site_survey = read.csv(site_survey_filename)
+
+# read environmental raster data
+env_rast = terra::rast(env_rast_filename)
+
+# format data for analysis
+site_pair = gdm::formatsitepair(bioData=site_survey,
+                                bioFormat=1,
+                                siteColumn='site',
+                                XColumn='x',
+                                YColumn='y',
+                                predData=env_rast)
+
+# fit GDM
+mod = gdm::gdm(site_pair, geo=F)
+print(summary(mod))
+
+# save I-splines
+splines = as.data.frame(isplineExtract(mod))
+write.csv(splines, spline_filename)
+
+# save first <=3 PCs of GDM-transformed env space
+env_rast_trans <- gdm::gdm.transform(model=mod, data=env_rast)
+pca_samp <- terra::prcomp(env_rast_trans, maxcell = 5e5)
+n_pcs = min(3, dim(env_rast)[3])
+pca_rast <- terra::predict(env_rast_trans, pca_samp, index=1:n_pcs)
+pca_rast <- terra::stretch(pca_rast)
+terra::writeRaster(pca_rast, pca_rast_filename, overwrite=T)
+cat("\nGDM OUTPUTS SAVED TO DISK.\n")
