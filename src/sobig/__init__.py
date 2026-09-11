456d2c6c615050524b0223d8e0cb04b6a63eb00d
Drew Terasaki Hart <ter066@csiro.au>
2026-09-11 17:50:27 +1000

    debug a bit. add overview image

--- src/sobig/__init__.py
+++ src/sobig/__init__.py
@@ -2,6 +2,6 @@
 
 __version__ = "0.1.0"
 
-from .sobig import fEnv, Species, Sim, run_demo
+from .main import fEnv, Species, Sim, run_demo
 
 # Public API exports will go here as the package stabilizes.
